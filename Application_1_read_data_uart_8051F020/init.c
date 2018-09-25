d8a3d80988302152e9fbb10a4a0bb64234ca2a27
AlexYZ <alexisartigues@hotmail.fr>
2018-09-25 15:11:03 +0200

    LED P1.6 working

---
+++ Application_1_read_data_uart_8051F020/init.c
@@ -0,0 +1,84 @@
+
+
+#include "C8051F020.h"
+#include "init.h"
+
+// Peripheral specific initialization functions,
+// Called from the Init_Device() function
+
+//-----------------------------------------------------------------------------
+// Dévalidation du watchdog
+//-----------------------------------------------------------------------------
+void Reset_Sources_Init()
+{
+    WDTCN     = 0xDE;
+    WDTCN     = 0xAD;
+}
+
+//-----------------------------------------------------------------------------
+// Configuration de l'oscillateur - SYSCLK = Clock externe 22118400Hz 
+//-----------------------------------------------------------------------------
+void Oscillator_Init()
+{
+    int i = 0;
+    OSCXCN    = 0x67;
+    for (i = 0; i < 3000; i++);  // Wait 1ms for initialization
+    while ((OSCXCN & 0x80) == 0);
+    OSCICN    = 0x0C;
+}
+
+
+
+//-----------------------------------------------------------------------------
+// Initialisation globale du Microcontrôleur - 
+//-----------------------------------------------------------------------------
+void Init_Device(void)
+{
+    Reset_Sources_Init();
+    Oscillator_Init();
+		PortIO_Init();
+		P1MDOUT |= 0x40; // Met la sortie 6 en sortie
+}
+
+void PortIO_Init(void){
+	    // P0.0  -  TX0 (UART0), Open-Drain, Digital
+    // P0.1  -  RX0 (UART0), Open-Drain, Digital
+    // P0.2  -  T0 (Timer0), Open-Drain, Digital
+    // P0.3  -  Unassigned,  Open-Drain, Digital
+    // P0.4  -  Unassigned,  Open-Drain, Digital
+    // P0.5  -  Unassigned,  Open-Drain, Digital
+    // P0.6  -  Unassigned,  Open-Drain, Digital
+    // P0.7  -  Unassigned,  Open-Drain, Digital
+
+    // P1.0  -  Unassigned,  Open-Drain, Digital
+    // P1.1  -  Unassigned,  Open-Drain, Digital
+    // P1.2  -  Unassigned,  Open-Drain, Digital
+    // P1.3  -  Unassigned,  Open-Drain, Digital
+    // P1.4  -  Unassigned,  Open-Drain, Digital
+    // P1.5  -  Unassigned,  Open-Drain, Digital
+    // P1.6  -  Unassigned,  Open-Drain, Digital
+    // P1.7  -  Unassigned,  Open-Drain, Digital
+
+    // P2.0  -  Unassigned,  Open-Drain, Digital
+    // P2.1  -  Unassigned,  Open-Drain, Digital
+    // P2.2  -  Unassigned,  Open-Drain, Digital
+    // P2.3  -  Unassigned,  Open-Drain, Digital
+    // P2.4  -  Unassigned,  Open-Drain, Digital
+    // P2.5  -  Unassigned,  Open-Drain, Digital
+    // P2.6  -  Unassigned,  Open-Drain, Digital
+    // P2.7  -  Unassigned,  Open-Drain, Digital
+
+    // P3.0  -  Unassigned,  Open-Drain, Digital
+    // P3.1  -  Unassigned,  Open-Drain, Digital
+    // P3.2  -  Unassigned,  Open-Drain, Digital
+    // P3.3  -  Unassigned,  Open-Drain, Digital
+    // P3.4  -  Unassigned,  Open-Drain, Digital
+    // P3.5  -  Unassigned,  Open-Drain, Digital
+    // P3.6  -  Unassigned,  Open-Drain, Digital
+    // P3.7  -  Unassigned,  Open-Drain, Digital
+
+    XBR0      = 0x04;
+    XBR1      = 0x02;
+    XBR2      = 0x40;
+}
+
